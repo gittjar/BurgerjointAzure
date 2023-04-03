09ae86b46e6c3054f8b99594d3651bd0ffecf1b3
gittjar <jarno.keranen@gmail.com>
2023-04-03 12:12:12 +0300

    updated products price

--- call-webapi-aspnet/Models/Burger.cs
+++ call-webapi-aspnet/Models/Burger.cs
@@ -4,6 +4,7 @@ namespace call_webapi_aspnet.Models
 	public class Burger
 	{
         public int? id { get; set; }
+        public string? hinta { get; set; }
         public string? title { get; set; }
         public string? description { get; set; }
         public string? information { get; set; }
